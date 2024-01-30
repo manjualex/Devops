5e95caae89fffc56036be27f1647feca0636498c
manju <manjualex55@gmail.com>
2024-01-30 17:10:55 -0600

    commit change

--- demo.py
+++ demo.py
@@ -1,12 +1,6 @@
 print("Hello, World!")
-<<<<<<< HEAD
-<<<<<<< HEAD
 this is the output
 this is the new output
-=======
 code change
->>>>>>> a790d9c (Update demo.py)
-=======
-this is the output
-this is the new output
->>>>>>> cadd66a (second changes done)
+
+
