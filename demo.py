fdd8f12a9e034ddcd788c2e112234cbb4ba37f04
manju <manjualex55@gmail.com>
2024-01-30 12:02:40 -0600

    second changes done

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 print("Hello, World!")
 <<<<<<< HEAD
+<<<<<<< HEAD
 this is the output
 this is the new output
 =======
 code change
 >>>>>>> a790d9c (Update demo.py)
+=======
+this is the output
+this is the new output
+>>>>>>> cadd66a (second changes done)
